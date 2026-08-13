037ff7135e5156b2a65c20220eba90a82c02bfb2
RivanFadlani <rivanfadlani7@gmail.com>
2026-08-13 13:43:03 +0700

    study: OS Module (130826)

---
+++ belajar-nodejs-dasar/os.mjs
@@ -0,0 +1,12 @@
+import os from "os";
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.uptime());
+
+console.table(os.cpus());
+console.table(os.networkInterfaces());
